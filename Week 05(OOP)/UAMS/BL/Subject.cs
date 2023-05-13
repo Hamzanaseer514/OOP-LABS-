44f0cbd2025bd5b62ac672db4196d603823d9d8f
Hamzanaseer514 <120157078+Hamzanaseer514@users.noreply.github.com>
2023-05-13 02:43:32 -0600

    Add files via upload

---
+++ Week 05(OOP)/UAMS/BL/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAMS.BL
+{
+    class Subject
+    {
+        public string subjectCode;
+        public float creditHours;
+        public string subjectType;
+        public double fees;
+        public Subject(string code,int hours,string name,double fees)
+        {
+            this.subjectCode = code;
+            this.creditHours = hours;
+            this.subjectType = name;
+            this.fees = fees;
+        }
+    }
+}
